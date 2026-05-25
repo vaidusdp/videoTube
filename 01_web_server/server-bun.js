8852105df1d7beb9886053df50afcaf39e1e15a4
vaidus2608m <vaidus2608m@gmail.com>
2026-05-25 14:04:50 +0530

    Building Server in node and bun

---
+++ 01_web_server/server-bun.js
@@ -0,0 +1,16 @@
+import {serve} from 'bun'
+
+serve({
+    fetch(request){
+        const url = new URL(request.url);
+        if(url.pathname === '/'){
+            return new Response('Home', {status: 200})
+        } else if(url.pathname === '/ice-tea'){
+            return new Response('Ice Tea', {status: 200})
+        } else {
+            return new Response('404 Not Found', {status: 404})
+        }
+    },
+    port: 3000,
+    hostname: '127.0.0.1'
+})
