b3b5c9957a09bb8afd9ef625503efdef486bf512
vaidus2608m <vaidus2608m@gmail.com>
2026-05-26 10:41:23 +0530

    Just messing around with winston and morgan

--- 02_express/index.js
+++ 02_express/index.js
@@ -1,15 +1,37 @@
 import 'dotenv/config'
 import express from "express";
+import logger from './logger.js';
+import morgan from 'morgan';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+const morganFormat = ':method :url :status :response-time ms'
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
+
 let teaData = [];
 let nextId = 1;
 
 app.post("/teas", (req, res) => {
+    logger.info("Post REQ:")
+    logger.warn("Warning:")
     const { name, price } = req.body;
 
     const newTea = {
